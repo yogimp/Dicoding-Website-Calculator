60d6b261766df70d0c16541cb1eef8220c428486
yogimp <yogimeizalputra@gmail.com>
2022-10-15 07:17:50 +0700

    Initialize some basic variable and function javascript

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+
+}
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll('.button');
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+      // mendapatkan objek elemen yang diklik
+      const target = event.target;
+   
+      inputDigit(target.innerText);
+      updateDisplay();
+    });
+}
