3b849721635fb3b2a4a73ec3585afafc4a738de3
yogimp <yogimeizalputra@gmail.com>
2022-10-15 13:02:46 +0700

    Adding some operator funtions

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,44 @@ function clearCalculator() {
     calculator.isWaitForSecondNumber = false;
 }
 function inputDigit(digit) {
-    calculator.displayNumber += digit;
+    if (calculator.displayNumber === '0') {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+}
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+      return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+function handleOperator(operator) {
+    if (!calculator.isWaitForSecondNumber) {
+      calculator.operator = operator;
+      calculator.isWaitForSecondNumber = true;
+      calculator.firstNumber = calculator.displayNumber;
+   
+      // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+      calculator.displayNumber = '0';
+    } else {
+      alert('Operator sudah ditetapkan');
+    }
+}
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+      alert('Anda belum menetapkan operator');
+      return;
+    }
+   
+    let result = 0;
+    if (calculator.operator === '+') {
+      result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+      result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+   
+    calculator.displayNumber = result;
 }
 
 const buttons = document.querySelectorAll('.button');
@@ -24,6 +61,27 @@ for (const button of buttons) {
     button.addEventListener('click', function (event) {
       // mendapatkan objek elemen yang diklik
       const target = event.target;
+   
+      if (target.classList.contains('clear')) {
+        clearCalculator();
+        updateDisplay();
+        return;
+      }
+   
+      if (target.classList.contains('negative')) {
+        inverseNumber();
+        updateDisplay();
+        return;
+      }
+      if (target.classList.contains('equals')) {
+        performCalculation();
+        updateDisplay();
+        return;
+      }
+      if (target.classList.contains('operator')) {
+        handleOperator(target.innerText);
+        return;
+      }
    
       inputDigit(target.innerText);
       updateDisplay();
